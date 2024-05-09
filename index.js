6a1a301ef23554788a0af431984bc3da52103341
tungbeo2225 <steeven866pun0suy@hotmail.com>
2024-05-09 08:44:52 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
