6c2f0bf51c55ced9f8ba6012427b41037d516d1a
tungbeo2225 <steeven866pun0suy@hotmail.com>
2024-05-11 13:54:00 +0700

    feat project

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const pullAtIndex = (arr, pullArr) => {
-  let removed = [];
-  let pulled = arr
-    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
-    .filter((v, i) => !pullArr.includes(i));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-  return removed;
-};
+function exist(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (dfs(board, i, j, word, 0)) return true;
+    }
+  }
+  return false;
+  function dfs(board, i, j, word, index) {
+    if (index === word.length) return true;
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
+      return false;
+    const temp = board[i][j];
+    board[i][j] = "#";
+    const found =
+      dfs(board, i + 1, j, word, index + 1) ||
+      dfs(board, i - 1, j, word, index + 1) ||
+      dfs(board, i, j + 1, word, index + 1) ||
+      dfs(board, i, j - 1, word, index + 1);
+    board[i][j] = temp;
+    return found;
+  }
+}
